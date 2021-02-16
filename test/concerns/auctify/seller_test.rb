fe8df0c0439fef186b040269c3b835936df7ce0b
foton <foton@centrum.cz>
2021-02-16 17:54:05 +0100

    raising error when trying sell unauctified item

--- test/concerns/auctify/seller_test.rb
+++ test/concerns/auctify/seller_test.rb
@@ -18,5 +18,15 @@ module Auctify
       assert SellerTestUser.new(name: "Krutibrko").respond_to?(:sales)
       assert SellerTestUser.new(name: "Krutibrko").respond_to?(:sell)
     end
+
+    test "#sell allow to sell only auctified items" do
+      sale = users(:adam).sell(things(:apple), in: :auction, price: 1_000)
+      assert sale.is_a?(Auctify::Sale::Base)
+
+      # TODO: `assert_raises(Auctify::NonAuctifiedItem) do`
+      assert_raises(RuntimeError) do
+        users(:adam).sell(CleanThing.new, in: :auction, price: 1_000)
+      end
+    end
   end
 end
