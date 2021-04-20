b3327e3771454e02b5a54babf85037d1ca085aaf
foton <foton@centrum.cz>
2021-04-20 15:15:48 +0200

    all tests green!

--- app/helpers/auctify/application_helper.rb
+++ app/helpers/auctify/application_helper.rb
@@ -3,5 +3,8 @@
 module Auctify
   module ApplicationHelper
     include Auctify::Engine.routes.url_helpers
+    # line above somehow breaks url generation in forms from main_app
+    # so we nedd to ensure main_app supremacy
+    include Rails.application.routes.url_helpers
   end
 end
