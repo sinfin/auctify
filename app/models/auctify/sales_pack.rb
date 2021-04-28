8dab6f76e285f1889d1c7e558522a1d35fadae8d
Petr Marek <contact@petrmarek.eu>
2021-04-28 10:50:33 +0200

    uncomment sales_pack items has_many

--- app/models/auctify/sales_pack.rb
+++ app/models/auctify/sales_pack.rb
@@ -8,7 +8,7 @@ module Auctify
     include Folio::Publishable::Basic
 
     has_many :sales, class_name: "Auctify::Sale::Base", foreign_key: :pack_id, inverse_of: :pack, dependent: :nullify
-    # has_many :items, through: :sales
+    has_many :items, through: :sales
 
     validates :title,
               presence: true,
@@ -20,12 +20,6 @@ module Auctify
                     against: %i[title],
                     ignoring: :accents,
                     using: { tsearch: { prefix: true } }
-
-    def items
-      @items ||= sales.collect(&:item) # TODO: make it Arel/Association like
-      # used_polymorhic_classes = sales.pluck(:item_type).uniq
-      # classes_with_table_names = used_polymorhic_classes.collect { |pc| [pc, pc.constantize.table_name] }
-    end
 
     def to_label
       title
