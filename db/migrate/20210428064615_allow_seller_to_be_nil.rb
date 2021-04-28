1283fababd241cb7dcf099c7ca5b697c0d7cfe81
foton <foton@centrum.cz>
2021-04-28 10:01:36 +0200

    removed console logs

--- db/migrate/20210428064615_allow_seller_to_be_nil.rb
+++ db/migrate/20210428064615_allow_seller_to_be_nil.rb
@@ -4,12 +4,10 @@ class AllowSellerToBeNil < ActiveRecord::Migration[6.0]
   def up
     change_column :auctify_sales, :seller_id, :integer, null: true
     change_column :auctify_sales, :seller_type, :string, null: true
-    puts("Maybe changed")
   end
 
   def down
     change_column :auctify_sales, :seller_id, :integer, null: false
     change_column :auctify_sales, :seller_type, :string, null: false
-    puts("Reverted")
   end
 end
