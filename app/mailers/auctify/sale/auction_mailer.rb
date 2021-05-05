402c347b12b69afd77bc2b1d037f0426757cfe9a
dedekm <dedekmm@gmail.com>
2021-05-05 21:38:54 +0200

    add admin notifications to auction_mailer

--- app/mailers/auctify/sale/auction_mailer.rb
+++ app/mailers/auctify/sale/auction_mailer.rb
@@ -15,6 +15,16 @@ class Auctify::Sale::AuctionMailer < ApplicationMailer
     email_template_mail(data, to: bid.bidder.email)
   end
 
+  def admin_new_bid(bid)
+    data = new_bid_data(bid).merge(admin_bid_data(bid))
+    email_template_mail(data)
+  end
+
+  def admin_new_bid_with_limit(bid)
+    data = new_bid_with_limit_data(bid).merge(admin_bid_data(bid))
+    email_template_mail(data)
+  end
+
   private
     def new_bid_data(bid)
       {
@@ -29,5 +39,13 @@ class Auctify::Sale::AuctionMailer < ApplicationMailer
       new_bid_data(bid).merge(
         BID_MAX_PRICE: ActionController::Base.helpers.number_to_currency(bid.max_price, precision: 0),
       )
+    end
+
+    def admin_bid_data(bid)
+      {
+        # TODO: add bidder number (registration number?)
+        BIDDER_NUMBER: bid.registration.id,
+        BIDDER_NAME: bid.bidder.try(:full_name),
+      }
     end
 end
