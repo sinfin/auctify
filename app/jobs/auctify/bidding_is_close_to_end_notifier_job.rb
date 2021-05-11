65d5bcf04a4b21e2405a51a85c11d7c641ada4d4
foton <foton@centrum.cz>
2021-05-11 09:37:01 +0200

    more verbose `BiddingIsCloseToEndNotifierJob`

--- app/jobs/auctify/bidding_is_close_to_end_notifier_job.rb
+++ app/jobs/auctify/bidding_is_close_to_end_notifier_job.rb
@@ -8,9 +8,17 @@ module Auctify
       auction = Auctify::Sale::Auction.find(auction_id)
       notify_time = auction.ends_at - Auctify.configuration.when_to_notify_bidders_before_end_of_bidding
 
+      for_auction_str = " for auction[#{auction.id}] ending at { orginal: #{auction.ends_at}, current: #{auction.currently_ends_at}}"
+
       if notify_time <= Time.current && auction.open_for_bids?
+        puts("PUTS: running `auction.before_bidding_is_close_to_end` #{for_auction_str}")
+        Rails.logger.info("LOGGER: running `auction.before_bidding_is_close_to_end` #{for_auction_str}")
+
         auction.before_bidding_is_close_to_end
       else
+        puts("PUTS: setting new invocation to #{notify_time} #{for_auction_str}")
+        Rails.logger.info("LOGGER: setting new invocation to #{notify_time} #{for_auction_str}")
+
         self.class.set(wait_until: notify_time)
                   .perform_later(auction_id: auction.id)
       end
